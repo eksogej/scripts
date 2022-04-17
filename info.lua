23524746fed602a2920ed44190041550dfa63769
ekso <103387505+eksogej@users.noreply.github.com>
2022-04-18 00:02:09 +0200

    Create info.lua

---
+++ info.lua
@@ -0,0 +1,17 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Puppyware Crack";
+    Text = "Hello, Loading...";
+    Icon = "http://www.roblox.com/asset/?id=8768441000";
+    Duration = "3";
+    callbakc = bindableFunction;
+    Button1 = "Okay!";
+})
+wait(5)
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Puppyware Crack";
+    Text = "Fixed By ekso.gq!";
+    Icon = "http://www.roblox.com/asset/?id=8768441000";
+    Duration = "3";
+    callbakc = bindableFunction;
+})
+wait(2)
